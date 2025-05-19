9aac7bcd0af480b15ef6d3925fca7aeab1c4991f
AYKupenov21 <AYKupenov21@codingburgas.bg>
2025-05-19 15:49:12 +0300

    Update poll.html file design.

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('dqwedqwdqw', 'super secret key')
-    SQLALCHEMY_DATABASE_URI = (f"mssql+pyodbc://DESKTOP-EVLE77T/proba?driver=ODBC+Driver+17+for+SQL+Server")
+    SQLALCHEMY_DATABASE_URI = (f"mssql+pyodbc://SD2357\SQLEXPRESS/proba?driver=ODBC+Driver+17+for+SQL+Server")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
