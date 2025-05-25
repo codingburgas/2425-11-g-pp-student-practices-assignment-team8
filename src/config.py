55a63084622953eecb0d89cf71b309c6af654d82
YDPenkov21 <YDPenkov21@codingburgas.bg>
2025-05-25 11:42:37 +0300

    Create website logo. Change the background and replace the old logo with the new one in the navigation bar.

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('dqwedqwdqw', 'super secret key')
-    SQLALCHEMY_DATABASE_URI = (f"mssql+pyodbc://MITAKA/proba?driver=ODBC+Driver+17+for+SQL+Server")
+    SQLALCHEMY_DATABASE_URI = (f"mssql+pyodbc://DESKTOP-EVLE77T/proba?driver=ODBC+Driver+17+for+SQL+Server")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
