8b270fdc510ec9d4b870ece6abb70fa206da4c40
KDIvanov21 <KDIvanov21@codingburgas.bg>
2025-06-16 12:34:15 +0300

    add comments

--- src/auth/forms.py
+++ src/auth/forms.py
@@ -2,17 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Email
 from wtforms.fields.simple import SubmitField, PasswordField, StringField, EmailField
 
+#Login
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+#Email
 class RegistrationForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+#Register
 class ProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
