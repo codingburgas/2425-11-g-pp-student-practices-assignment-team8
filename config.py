adaa5809604f3d4bd3920575b16abebfe6f45b16
KDIvanov21 <KDIvanov21@codingburgas.bg>
2025-06-16 12:12:29 +0300

    Ease config

--- config.py
+++ config.py
@@ -14,13 +14,11 @@ class Config:
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
 
-    # Azure SQL Database connection
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_SERVER = os.getenv('DB_SERVER')
     DB_NAME = os.getenv('DB_NAME')
 
-    # Construct the connection string for Azure SQL Database with the correct driver name
     SQLALCHEMY_DATABASE_URI = (
         f'mssql+pyodbc://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:1433/{DB_NAME}'
         '?driver=ODBC+Driver+17+for+SQL+Server'
@@ -30,7 +28,6 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # JWT Configuration
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret-key')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
