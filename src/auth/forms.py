7affb5ce3ed8f2d86dab684d7e98239ee4242b96
AYKupenov21 <AYKupenov21@codingburgas.bg>
2025-07-01 22:15:41 +0300

    Add two new libraries to the requirements and create requirements to the password.

--- src/auth/forms.py
+++ src/auth/forms.py
@@ -1,6 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Email
 from wtforms.fields.simple import SubmitField, PasswordField, StringField, EmailField
+from wtforms.validators import ValidationError
+import re
+
+def validate_strong_password(form, field):
+    password = field.data
+    if len(password) < 8:
+        raise ValidationError('Password must be at least 8 characters long.')
+    if not re.search(r'[A-Z]', password):
+        raise ValidationError('Password must include at least one uppercase letter.')
+    if not re.search(r'[a-z]', password):
+        raise ValidationError('Password must include at least one lowercase letter.')
+    if not re.search(r'[0-9]', password):
+        raise ValidationError('Password must include at least one number.')
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        raise ValidationError('Password must include at least one special character.')
 
 #Login
 class LoginForm(FlaskForm):
@@ -12,11 +27,10 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), validate_strong_password])
     submit = SubmitField('Register')
 
-#Register
 class ProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), validate_strong_password])
     submit = SubmitField('Update')
